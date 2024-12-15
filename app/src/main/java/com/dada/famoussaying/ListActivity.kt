df2404a1498ef28b586fdfad875fcb5e44d28317
dahyeon777 <daaaa0727@gmail.com>
2024-12-16 00:12:14 +0900

    Add RecyclerView and QuoteAdapter2

--- app/src/main/java/com/dada/famoussaying/ListActivity.kt
+++ app/src/main/java/com/dada/famoussaying/ListActivity.kt
@@ -4,21 +4,35 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.dada.famoussaying.databinding.ActivityListBinding
+import com.dada.famoussaying.db.Quote
+
 
 class ListActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_list)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_list)
 
-        //홈 이동
+        // RecyclerView 설정
+        val recyclerView = binding.quoteRecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // 데이터 생성
+        val quotes = listOf(
+            Quote(0, "산다는것 그것은 치열한 전투이다.", "로망로랑","2024-12-15")
+        )
+
+        // Adapter 설정
+        recyclerView.adapter = QuoteAdapter(quotes)
+
+        // 홈 이동 버튼 클릭 리스너
         binding.homeBtn.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
-
-
     }
 }
--- app/src/main/java/com/dada/famoussaying/db/QuoteEntity.kt
+++ app/src/main/java/com/dada/famoussaying/db/QuoteEntity.kt
@@ -8,5 +8,5 @@ data class Quote(
     @PrimaryKey(autoGenerate = true) var id: Int,
     val content: String,
     val name: String,
-    val date: Long
+    val date: String
 )
