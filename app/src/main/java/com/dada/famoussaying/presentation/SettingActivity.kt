77cfc294b8c9e6380feb13046e04ae601fca36f1
dahyeon777 <daaaa0727@gmail.com>
2025-01-16 16:42:39 +0900

    Add Notification

--- app/src/main/java/com/dada/famoussaying/presentation/SettingActivity.kt
+++ app/src/main/java/com/dada/famoussaying/presentation/SettingActivity.kt
@@ -1,12 +1,79 @@
 package com.dada.famoussaying.presentation
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
 import com.dada.famoussaying.R
+import com.dada.famoussaying.databinding.ActivityMainBinding
+import com.dada.famoussaying.databinding.ActivitySettingBinding
 
 class SettingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySettingBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setting)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_setting)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                    101
+                )
+            }
+        }
+
+        fun createNotificationChannel(context: Context) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val channelId = "default_channel_id"
+                val channelName = "Default Channel"
+                val importance = NotificationManager.IMPORTANCE_DEFAULT
+                val channel = NotificationChannel(channelId, channelName, importance)
+                channel.description = "This is the default notification channel."
+
+                val notificationManager =
+                    context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                notificationManager.createNotificationChannel(channel)
+            }
+        }
+
+        fun showNotification(context: Context) {
+            val channelId = "default_channel_id" // 위에서 설정한 채널 ID
+            val notificationId = 1
+
+            val builder = NotificationCompat.Builder(context, channelId)
+                .setSmallIcon(android.R.drawable.ic_dialog_info) // 알림 아이콘
+                .setContentTitle("알림앱") // 알림 제목
+                .setContentText("명언~~~~~~~~~~~~~~~~~~~~~~~~~~~~") // 알림 내용
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT) // 알림 우선순위
+
+            with(NotificationManagerCompat.from(context)) {
+                notify(notificationId, builder.build())
+            }
+        }
+
+        createNotificationChannel(this) // 앱 시작 시 채널 생성
+
+        binding.noticeBtn.setOnClickListener {
+            showNotification(this)
+        }
+
     }
 }
